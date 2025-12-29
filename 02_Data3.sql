c469f9d128e8ea2efbec49e4a17a99d77f75d9f9
OscarFdz24 <chinchillalopezfernandez@gmail.com>
2025-12-29 16:17:05 +0100

    Archivo de actividad de pois corregido. Fechas diferentes en el intervalo de 1 año

--- 02_Data3.sql
+++ 02_Data3.sql
@@ -1,21 +1,26 @@
--- 03_fact_simple.sql
 USE `pois`;
 
 START TRANSACTION;
 
+-- Limpieza previa
 DELETE FROM fact_poi_activity;
 
+-- Inserción de hechos con fechas distribuidas a lo largo del año
 INSERT INTO fact_poi_activity
 (poi_id, activity_date, visits, revenue_eur, avg_rating)
 SELECT
   poi_id,
-  '2025-01-01',
+  DATE_ADD(
+    '2024-01-01',
+    INTERVAL FLOOR(RAND() * 365) DAY
+  ) AS activity_date,
   FLOOR(RAND() * 50),
   ROUND(RAND() * 500, 2),
   ROUND(2.5 + RAND() * 2.5, 2)
 FROM dim_poi
 LIMIT 5000;
 
+-- Limpieza de valores no válidos
 UPDATE fact_poi_activity
 SET avg_rating = NULL
 WHERE visits = 0;
